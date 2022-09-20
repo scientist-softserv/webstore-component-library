1f19154dd3099ed24f7e728f1b61ae1a7c58ea87
Alisha Evans <alisha@notch8.com>
2022-09-20 17:52:11 -0500

    refactor stories

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
-import Button from './Button'
+import Button from '../Button'
 import './header.css'
 
 const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Button from './Button'
+import Button from '../components/Button'
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
