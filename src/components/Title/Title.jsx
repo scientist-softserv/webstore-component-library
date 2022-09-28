e64bb4bff048fa14314feca3c2821e3540b8f33a
Alisha Evans <alisha@notch8.com>
2022-09-28 13:07:10 -0500

    add the title component and story

---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './title.css'
+
+const Title = ({ size, label, styles, ...props }) => {
+	return (
+		<p
+			className={`title title--${size}`}
+			style={{ ...styles }}
+			{...props}
+		>
+			{label}
+		</p>
+	)
+}
+
+Title.propTypes = {
+	size: PropTypes.oneOf(['small', 'medium', 'large']),
+	label: PropTypes.string.isRequired,
+	styles: PropTypes.object,
+}
+
+Title.defaultProps = {
+	size: 'large',
+	label: 'This is a title.'
+}
+
+export default Title
---
+++ src/components/index.jsx
@@ -0,0 +1,5 @@
+import Title from './Title/Title'
+
+export {
+	Title,
+}
