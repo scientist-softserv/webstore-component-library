70c89704924cc72e1b5c75341d42fad89f9c08fe
Alisha Evans <alisha@notch8.com>
2022-10-04 11:50:16 -0500

    allow user to have 3 or 4 columns in featured services

--- src/compounds/FeaturedServices/FeaturedServices.jsx
+++ src/compounds/FeaturedServices/FeaturedServices.jsx
@@ -3,22 +3,31 @@ import PropTypes from 'prop-types'
 import { FeaturedService, Title } from '../../components'
 import './featured-services.css'
 
-const FeaturedServices = ({ services }) => (
-	<section className='featured-services'>
-		<Title size='medium' title='Featured Services' />
-		<div className='services-wrapper'>
-			{services.map((service) => <FeaturedService key={service.id} {...service} />)}
-		</div>
-	</section>
-)
+const FeaturedServices = ({ group_by, services }) => {
+
+	return (
+		<section className='featured-services'>
+			<Title size='medium' title='Featured Services' />
+			<div className={`services-wrapper--${group_by}`}>
+				{services.map((service) => <FeaturedService key={service.id} {...service} />)}
+			</div>
+		</section>
+	)
+}
 
 FeaturedServices.propTypes = {
+	group_by: PropTypes.oneOf([3, 4]),
 	services: PropTypes.arrayOf(PropTypes.shape({
 		description: PropTypes.string.isRequired,
-		imgProps: PropTypes.shape({}).isRequired,
+		img: PropTypes.shape({}).isRequired,
+		imgProps: PropTypes.shape({}),
 		name: PropTypes.string.isRequired,
-		styles: PropTypes.shape({}),
+		style: PropTypes.shape({}),
 	})).isRequired,
+}
+
+FeaturedServices.defaultProps = {
+	group_by: 3,
 }
 
 export default FeaturedServices
--- src/stories/FeaturedServices.stories.jsx
+++ src/stories/FeaturedServices.stories.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { FeaturedServices } from '../compounds'
 import { services } from '../resources/args'
 
+const four_services = services
+const three_services = services.filter((_s, index) => index < 3 )
+
 export default {
 	title: 'Compounds/FeaturedServices',
 	component: FeaturedServices,
@@ -11,7 +14,19 @@ export default {
 	// },
 }
 
-export const Default = (args) => <FeaturedServices {...args} />
+console.log('three_services', three_services )
+console.log('four_services', four_services )
+
+const Template = (args) => <FeaturedServices {...args} />
+
+export const Default = Template.bind({})
 Default.args = {
-	services,
+	services: three_services,
+	group_by: 3,
+}
+
+export const Alternate = (args) => <FeaturedServices {...args} />
+Alternate.args = {
+	services: four_services,
+	group_by: 4,
 }
