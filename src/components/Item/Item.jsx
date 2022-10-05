92196d8536a0f7b5bf1789056f5e1383a7379e0c
Alisha Evans <alisha@notch8.com>
2022-10-05 11:37:44 -0500

    Item can display a button

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,46 +1,73 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Button from '../Button/Button'
 import './item.css'
 
-const Item = ({ description, img, imgProps, orientation, name, style }) => {
+const Item = ({ buttonProps, description, img, imgProps, onClick, orientation, name, style, withButton }) => {
+	const { primary, backgroundColor, size, label } = buttonProps
 	const { alt, src } = img
 
 	return (
-		<div
-			className={`item-wrapper item-${orientation}`}
+		<article
+			className={`item-container item-${orientation}`}
 			style={{ ...style }}
 		>
 			<img className={`item-image item-image-${orientation}`} src={src} alt={alt} {...imgProps} />
-			<div className='item'>
-				<h3 className='item-name'>
-					{name}
-				</h3>
-				{description && (
-					<p className='item-description'>
-						{description}
-					</p>
+			<div className={`item-options-${orientation}`}>
+				<div className='item-details'>
+					<h3 className={`item-name ${!withButton && `item-name-button`}`} onClick={withButton ? undefined : onClick}>
+						{name}
+					</h3>
+					{description && (
+						<p className='item-description'>
+							{description}
+						</p>
+					)}
+				</div>
+				{withButton && (
+					<div className={`item-button-${orientation}`}>
+						<Button
+							primary={primary}
+							backgroundColor={backgroundColor}
+							size={size}
+							label={label}
+							onClick={withButton ? onClick : undefined}
+							{...buttonProps}
+						/>
+					</div>
 				)}
 			</div>
-		</div>
+		</article>
 	)
 }
 
 Item.propTypes = {
+	buttonProps: PropTypes.shape({
+		primary: PropTypes.bool,
+		backgroundColor: PropTypes.string,
+		size: PropTypes.oneOf(['small', 'medium', 'large']),
+		label: PropTypes.string.isRequired,
+	}),
 	description: PropTypes.string,
 	img: PropTypes.shape({
 		src: PropTypes.string.isRequired,
 		alt: PropTypes.string.isRequired,
 	}).isRequired,
 	imgProps: PropTypes.shape({}),
+	onClick: PropTypes.func,
 	orientation: PropTypes.oneOf(['horizontal', 'vertical']),
 	name: PropTypes.string.isRequired,
 	style: PropTypes.shape({}),
+	withButton: PropTypes.bool
 }
 
 Item.defaultProps = {
+	buttonProps: {},
 	description: '',
+	onClick: undefined,
 	orientation: 'vertical',
 	style: {},
+	withButton: false,
 }
 
 export default Item
--- src/stories/Item.stories.jsx
+++ src/stories/Item.stories.jsx
@@ -11,21 +11,36 @@ const Template = (args) => <Item {...args} />
 
 export const Default = Template.bind({})
 Default.args = {
+	buttonProps: {},
 	description: '',
 	img: {
 		src: featuredService,
 		alt: 'Several rows of test tubes with a liquid being put into one.',
 	},
+	imgProps: {},
+	onClick: () => console.log(`You clicked the ITEM!`),
+	orientation: 'vertical',
 	name: 'Test Tubes',
+	style: {},
+	withButton: false,
 }
 
 export const Alternate = Template.bind({})
 Alternate.args = {
+	buttonProps:{
+		backgroundColor: 'grey',
+		label: 'Request this item',
+		primary: true,
+	},
 	description: 'Test the presence of viable microorganisms.',
 	img: {
 		src: featuredService,
 		alt: 'Several rows of test tubes with a liquid being put into one.',
 	},
+	imgProps: {},
+	onClick: () => console.log(`You clicked the BUTTON!`),
 	orientation: 'horizontal',
 	name: 'Microbiological Sterility Testing',
+	style: {},
+	withButton: true,
 }
