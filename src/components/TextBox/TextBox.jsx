79188ea50605f0e001f0d149a2c47f04af2301b0
Alisha Evans <alisha@notch8.com>
2022-10-06 11:34:04 -0500

    create the TextBox component

---
+++ src/components/TextBox/TextBox.jsx
@@ -0,0 +1,39 @@
+/* eslint react/no-array-index-key: 0 */
+import React from 'react'
+import PropTypes from 'prop-types'
+import './text-box.css'
+
+const TextBox = ({ alignment, size, style, text }) => {
+	// account for Unix and Windows devices
+	const paragraphs = text.split(/\n+\t?\r?/)
+
+	return (
+		<>
+			{paragraphs.map((par, index) => (
+				<div className={`text-box--${alignment}`} key={index}>
+					<p
+						className={`text-box text-box--${size}`}
+						style={{ ...style }}
+					>
+						{par}
+					</p>
+				</div>
+			))}
+		</>
+	)
+}
+
+TextBox.propTypes = {
+	alignment: PropTypes.oneOf(['left', 'center', 'right']),
+	size: PropTypes.oneOf(['small', 'medium', 'large']),
+	style: PropTypes.shape({}),
+	text: PropTypes.string.isRequired,
+}
+
+TextBox.defaultProps = {
+	alignment: 'left',
+	size: 'small',
+	style: {},
+}
+
+export default TextBox
