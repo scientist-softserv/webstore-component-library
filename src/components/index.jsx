0790c5aecfe249cc90c9462b06130bb38e4f94b9
Alisha Evans <alisha@notch8.com>
2022-10-06 12:23:58 -0500

    update all references to Button so that Item can render correctly

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,8 +1,10 @@
+import Button from './Button/Button'
 import Item from './Item/Item'
 import TextBox from './TextBox/TextBox'
 import Title from './Title/Title'
 
 export {
+	Button,
 	Item,
 	TextBox,
 	Title,
