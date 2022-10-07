ef703275aa27fc6532e99cba0a0ebfaf8c09a014
Alisha Evans <alisha@notch8.com>
2022-10-07 13:18:27 -0500

    bring the logo and navlink components into the header

--- src/compounds/index.jsx
+++ src/compounds/index.jsx
@@ -1,8 +1,10 @@
+import Header from './Header/Header'
 import Logo from './Logo/Logo'
 import ItemGroup from './ItemGroup/ItemGroup'
 import TitledTextBox from './TitledTextBox/TitledTextBox'
 
 export {
+	Header,
 	Logo,
 	ItemGroup,
 	TitledTextBox,
