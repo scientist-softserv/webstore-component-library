3ceca0d7ae2e4e6e45c571d2c9a3680e28cf9047
Alisha Evans <alisha@notch8.com>
2022-10-07 13:16:34 -0500

    create the logo compound

--- src/compounds/index.jsx
+++ src/compounds/index.jsx
@@ -1,7 +1,9 @@
+import Logo from './Logo/Logo'
 import ItemGroup from './ItemGroup/ItemGroup'
 import TitledTextBox from './TitledTextBox/TitledTextBox'
 
 export {
+	Logo,
 	ItemGroup,
 	TitledTextBox,
 }
