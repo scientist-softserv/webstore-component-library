b8d82e6eb50ca6a5d835cd0710870a2683cabcdc
Alisha Evans <alisha@notch8.com>
2022-10-11 16:44:52 -0500

    create a sample Home Page story

---
+++ src/stories/Home.stories.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { LoggedOut } from './Header.stories'
+import { Default as SearchBar } from './SearchBar.stories'
+import { Default as TitledTextBox } from './TitledTextBox.stories'
+import { Alternate as Hero } from './Image.stories'
+import { Alternate as ItemGroup } from './ItemGroup.stories'
+import { Alternate as Footer } from './Footer.stories'
+
+const Home = () => (
+	<main>
+		<LoggedOut {...LoggedOut.args} />
+		<Hero {...Hero.args} />
+		<SearchBar {...SearchBar.args} />
+		<TitledTextBox {...TitledTextBox.args} />
+		<ItemGroup {...ItemGroup.args} />
+		<Footer {...Footer.args} />
+	</main>
+)
+
+
+export default {
+	title: 'Example/Home Page',
+	component: Home,
+	parameters: {
+		layout: 'fullscreen',
+	},
+}
+
+export const HomePage = () => <Home />
