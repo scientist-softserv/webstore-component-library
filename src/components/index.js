05006d8b6402390a5888a6049ef1b90dcad77d6d
Alisha Evans <alisha@notch8.com>
2022-10-13 16:32:34 -0500

    rename .jsx extensions to .js for files that are not creating or using react components

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,6 @@
 import Button from './Button/Button'
 import Image from './Image/Image'
-import Item from './Item/Item'
 import Link from './Link/Link'
-import NavLink from './NavLink/NavLink'
 import SearchBar from './SearchBar/SearchBar'
 import SocialIcon from './SocialIcon/SocialIcon'
 import TextBox from './TextBox/TextBox'
@@ -11,9 +9,7 @@ import Title from './Title/Title'
 export {
 	Button,
 	Image,
-	Item,
 	Link,
-	NavLink,
 	SearchBar,
 	SocialIcon,
 	TextBox,
--- src/compounds/index.js
+++ src/compounds/index.js
@@ -1,15 +1,19 @@
 import Footer from './Footer/Footer'
 import Header from './Header/Header'
+import Item from './Item/Item'
+import ItemGroup from './ItemGroup/ItemGroup'
 import LinkGroup from './LinkGroup/LinkGroup'
 import Logo from './Logo/Logo'
-import ItemGroup from './ItemGroup/ItemGroup'
+import NavLink from './NavLink/NavLink'
 import TitledTextBox from './TitledTextBox/TitledTextBox'
 
 export {
 	Footer,
 	Header,
+	Item,
+	ItemGroup,
 	LinkGroup,
 	Logo,
-	ItemGroup,
+	NavLink,
 	TitledTextBox,
 }
