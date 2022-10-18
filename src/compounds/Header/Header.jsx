86bd3cfe47edde0bbdc08e59b1ccca3f3e3f9803
Alisha Evans <alisha@notch8.com>
2022-10-18 15:28:53 -0500

    pass an alt element to the Logo component since it is required in Image

--- src/compounds/Header/Header.jsx
+++ src/compounds/Header/Header.jsx
@@ -4,28 +4,35 @@ import NavLink from '../NavLink/NavLink'
 import Logo from '../Logo/Logo'
 import './header.css'
 
-//  may come back to hard code these
+// may come back to hard code these
 
-const Header = ({ browseLink, logInLink, logo, logOutLink, requestsLink, user }) => (
-	<header>
-		<div className='container header-container'>
-			<Logo logo={logo} />
-			<div>
-				<NavLink href={browseLink} label='Browse' />
-				<NavLink href={requestsLink} label='Requests' />
-				{user ? (
-					<NavLink href={logOutLink} label='Log Out' />
-				) : (
-					<NavLink href={logInLink} label='Log In' />
-				)}
+const Header = ({ browseLink, logInLink, logo, logOutLink, requestsLink, user }) => {
+	const { img, alt } = logo
+
+	return (
+		<header>
+			<div className='container header-container'>
+				<Logo img={img} alt={alt} />
+				<div>
+					<NavLink href={browseLink} label='Browse' />
+					<NavLink href={requestsLink} label='Requests' />
+					{user ? (
+						<NavLink href={logOutLink} label='Log Out' />
+					) : (
+						<NavLink href={logInLink} label='Log In' />
+					)}
+				</div>
 			</div>
-		</div>
-	</header>
-)
+		</header>
+	)
+}
 
 Header.propTypes = {
 	browseLink: PropTypes.string.isRequired,
-	logo: PropTypes.string.isRequired,
+	logo: PropTypes.shape({
+		img: PropTypes.string.isRequired,
+		alt: PropTypes.string.isRequired,
+	}).isRequired,
 	logInLink: PropTypes.string.isRequired,
 	logOutLink: PropTypes.string.isRequired,
 	requestsLink: PropTypes.string.isRequired,
