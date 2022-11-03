f9e42912e016fd6571ae79fccc150be3b1560c5d
Alisha Evans <alisha@notch8.com>
2022-11-03 18:11:46 -0500

    better argument destructuring in the Item component

--- src/compounds/Item/Item.jsx
+++ src/compounds/Item/Item.jsx
@@ -7,8 +7,8 @@ import './item.css'
 
 const Item = React.forwardRef(({ buttonLink, buttonProps, imgProps, item, orientation, style, titleLink, withButtonLink, withTitleLink,
 	href }, ref) => {
-	const { id, description, img, name } = item
-	const { alt, src } = img
+	const { id, description, name } = item
+	const { alt, src } = item.img
 
 	// "href" will apply when this component is being rendered from the ItemGroup
 	if (withButtonLink) buttonLink = buttonLink || href
