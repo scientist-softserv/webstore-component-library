02f558d4cce1b032f589da84fb697ffbe1d2ff1c
Alisha Evans <alisha@notch8.com>
2022-11-03 17:09:58 -0500

    update the Logo story arguments to work with the updated component arguments

--- src/compounds/Logo/Logo.stories.jsx
+++ src/compounds/Logo/Logo.stories.jsx
@@ -16,11 +16,11 @@ const Template = (args) => <Logo {...args} />
 export const Default = Template.bind({})
 Default.args = {
 	alt: 'The full logo for this company',
-	logo: logoFull,
+	src: logoFull,
 }
 
 export const Alternate = Template.bind({})
 Alternate.args = {
 	alt: 'The pictorial mark for this company',
-	logo: logoPictorial,
+	src: logoPictorial,
 }
