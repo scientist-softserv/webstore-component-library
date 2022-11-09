487a2a7e62291ea9c25441dc10d4a1e2477adc52
Alisha Evans <alisha@notch8.com>
2022-11-09 14:20:06 -0600

    initial rendering of ItemPage

---
+++ src/compounds/ItemPage/ItemPage.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Image from '../../components/Image/Image'
+import TextBox from '../../components/TextBox/TextBox'
+import Title from '../../components/Title/Title'
+import './item-page.css'
+
+const ItemPage = ({ title, titleStyle, description, descriptionStyle, img }) => {
+	return (
+		<main>
+			<Title title={title} style={titleStyle} />
+			<div className='item-page-details'>
+				<TextBox text={description} style={descriptionStyle} />
+				<Image {...img} />
+			</div>
+		</main>
+	)
+}
+
+ItemPage.propTypes = {
+	description: PropTypes.string.isRequired,
+	descriptionStyle: PropTypes.shape({}),
+	img: Image.defaultProps.isRequired,
+	title: PropTypes.string.isRequired,
+	titleStyle: PropTypes.shape({}),
+}
+
+ItemPage.defaultProps = {
+	descriptionStyle: {},
+	titleStyle: {},
+}
+
+export default ItemPage
