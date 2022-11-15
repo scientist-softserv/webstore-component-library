a060b93aecb9bce58fd2efa288515918923ca229
Alisha Evans <alisha@notch8.com>
2022-11-15 14:33:43 -0600

    create the Loading component

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { TailSpin } from 'react-loader-spinner'
+
+const Loading = ({ color, height, radius, width, ...props }) => (
+  <TailSpin
+    ariaLabel='tail-spin-loading'
+    color={color}
+    height={height}
+    width={width}
+    radius={radius}
+    visible={true}
+    {...props}
+  />
+)
+
+Loading.propTypes = {
+  color: PropTypes.string,
+  height: PropTypes.number,
+  width: PropTypes.number,
+  radius: PropTypes.number,
+  wrapperStyle: PropTypes.shape({}),
+  wrapperClass: PropTypes.string,
+}
+
+Loading.defaultProps = {
+  color: '#6a6a6a',
+  height: 40,
+  width: 40,
+  radius: 1,
+}
+
+export default Loading
