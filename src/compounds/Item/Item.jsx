578f5106a3a67ad7d70554e7a9c295cdf4239733
Alisha Evans <alisha@notch8.com>
2022-11-17 10:57:44 -0600

    use the custom margin classes in a few components

--- src/compounds/Item/Item.jsx
+++ src/compounds/Item/Item.jsx
@@ -40,17 +40,17 @@ const Item = React.forwardRef(({ buttonLink, buttonProps, imgProps, isLoading, i
         <div className='item-details'>
           {withTitleLink ? (
             <a href={titleLink} ref={ref} className='pointer-cursor item-link'>
-              <h3 className='item-name'>
+              <h3 className='item-name margin-top'>
                 {name}
               </h3>
             </a>
           ) : (
-            <h3 className='item-name'>
+            <h3 className='item-name margin-top'>
               {name}
             </h3>
           )}
           {description && (
-            <p className='item-description'>
+            <p className='item-description margin-vertical'>
               {/* TODO(alishaevn): account for really long descriptions */}
               {description}
             </p>
