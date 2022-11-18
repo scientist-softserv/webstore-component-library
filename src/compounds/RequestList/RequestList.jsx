7412a9236743d6f02fc5997fdd5fd5161cf43a31
Alisha Evans <alisha@notch8.com>
2022-11-18 12:45:50 -0600

    handle the loading state in the request list and update the story props

--- src/compounds/RequestList/RequestList.jsx
+++ src/compounds/RequestList/RequestList.jsx
@@ -1,24 +1,30 @@
 import React from 'react'
 import Link from 'next/link'
 import PropTypes from 'prop-types'
+import Loading from '../../components/Loading/Loading'
 import Title from '../../components/Title/Title'
 import RequestItem from '../RequestItem/RequestItem'
 
-const RequestList = ({ requests }) => (
+const RequestList = ({ requests, isLoading }) => (
   <>
     <Title title='My Requests' size='medium' />
-    {requests.map((req) => (
-      <Link key={req.id} href={`${req.identifier}`} passHref legacyBehavior>
-        <RequestItem
-          createdAt={req.createdAt}
-          description={req.description}
-          img={req.img}
-          title={`${req.identifier}: ${req.name}`}
-          status={req.status}
-          updatedAt={req.updatedAt}
-        />
-      </Link>
-    ))}
+    {isLoading
+    ? (
+      <Loading />
+    ) : (
+      requests.map((req) => (
+        <Link key={req.id} href={`${req.href}`} passHref legacyBehavior>
+          <RequestItem
+            createdAt={req.createdAt}
+            description={req.description}
+            img={req.img}
+            title={req.title}
+            status={req.status}
+            updatedAt={req.updatedAt}
+          />
+        </Link>
+      ))
+    )}
   </>
 )
 
