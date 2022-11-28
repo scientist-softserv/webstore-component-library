b5eb1efb5a623adae6629a4cdf5e2902a3842822
Summer Cook <summer@scientist.com>
2022-11-28 12:04:21 -0600

    refactor props

--- src/components/Document/Document.jsx
+++ src/components/Document/Document.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Document = ({ id, date, documentStatusColor, documentType, documentTypeColor, documentStatus, subtotalPrice }) => (
-  <div className='d-flex border rounded mb-2 bg-light'>
-    <div className='d-flex align-items-center'>
-      <div className='border-end p-2'>
-        <b className='fs-5'>{id}:</b> {subtotalPrice}
+const Document = (props) => {
+  const { id, date, documentStatusColor, documentType, documentTypeColor, documentStatus, subtotalPrice } = props
+
+  return (
+    <div className='d-flex border rounded mb-2 bg-light'>
+      <div className='d-flex align-items-center'>
+        <div className='border-end p-2'>
+          <b className='fs-5'>{id}:</b> {subtotalPrice}
+        </div>
+        <small className='text-muted fw-light p-2'>
+          {date}
+        </small>
       </div>
-      <small className='text-muted fw-light p-2'>
-        {date}
-      </small>
+      <div className='ms-auto p-2'>
+        <div className={`badge p-2 ${documentStatusColor}`}>
+          {documentStatus}
+        </div>
+        <div className={`badge p-2 ms-2 text-uppercase ${documentTypeColor}`}>
+          {documentType}
+        </div>
+      </div>
     </div>
-    <div className='ms-auto p-2'>
-      <div className={`badge p-2 ${documentStatusColor}`}>
-        {documentStatus}
-      </div>
-      <div className={`badge p-2 ms-2 text-uppercase ${documentTypeColor}`}>
-        {documentType}
-      </div>
-    </div>
-  </div>
-)
+  )
+}
 
 Document.propTypes = {
   id: PropTypes.string.isRequired,
