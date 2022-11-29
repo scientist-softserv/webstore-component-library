6e8c7b7a9bc3976246b8a93c0d4ff587fcfb5632
Summer Cook <summer@scientist.com>
2022-11-29 15:02:37 -0600

    update request stats

--- src/compounds/RequestStats/RequestStats.stories.jsx
+++ src/compounds/RequestStats/RequestStats.stories.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import RequestStats from './RequestStats'
+import { shipTo } from '../../resources/args'
 
 export default {
   title: 'Compounds/RequestStats',
@@ -10,9 +11,9 @@ const Template = (args) => <RequestStats {...args} />
 
 export const Default = Template.bind({})
 Default.args = {
-  billingAddress: '505 Lomas Sante Fe Dr\nSuite 110\nSolana Beach, CA, 92075\nUnited States',
+  billingAddress: shipTo.text,
   createdAt: 'September 9, 2022',
   deadline: 'September 17, 2022',
   projectCode: '',
-  shippingAddress: '505 Lomas Sante Fe Dr\nSuite 110\nSolana Beach, CA, 92075\nUnited States',
+  shippingAddress: shipTo.text,
 }
