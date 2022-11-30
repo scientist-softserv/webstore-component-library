2fe8d5c8d64fb70effa6e895f84d1059016ddca2
Alisha Evans <alisha@notch8.com>
2022-11-30 10:13:28 -0600

    add state to ActionsGroup so we know which action to show when an action button is clicked. update the handleShow and handleClose functions to account for this new action state variable too.

--- src/compounds/ActionsGroup/ActionsGroup.jsx
+++ src/compounds/ActionsGroup/ActionsGroup.jsx
@@ -4,17 +4,26 @@ import SendMessage from './actions/SendMessage'
 
 const ActionsGroup = () => {
   const [show, setShow] = useState(false)
-  const handleClose = () => setShow(false)
-  const handleShow = () => setShow(true)
+  const [action, setAction] = useState(null)
+
+  const handleShow = (action) => {
+    setAction(action)
+    setShow(true)
+  }
+
+  const handleClose = () => {
+    setAction(null)
+    setShow(false)
+  }
 
   return (
     <>
       <ListGroup>
-        <ListGroup.Item as='button' onClick={() => handleShow()}>
+        <ListGroup.Item as='button' onClick={() => handleShow('SendMessage')}>
           Send Message
         </ListGroup.Item>
       </ListGroup>
-      {show && <SendMessage handleClose={handleClose} />}
+      {(action === 'SendMessage' && show) && <SendMessage handleClose={handleClose} />}
     </>
   )
 }
