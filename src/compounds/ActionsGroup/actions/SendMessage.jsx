ab900f01c57eec87798c2812753426db06fc57db
Alisha Evans <alisha@notch8.com>
2022-11-30 17:11:26 -0600

    set up styles for the actions components

--- src/compounds/ActionsGroup/actions/SendMessage.jsx
+++ src/compounds/ActionsGroup/actions/SendMessage.jsx
@@ -1,6 +1,4 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button'
-import Modal from 'react-bootstrap/Modal'
+import './styles.scss'
 
 const SendMessage = ({ handleClose }) => (
   <Modal show={true} onHide={handleClose}>
