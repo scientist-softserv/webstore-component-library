07ca9bd238611e5ed8635e44611689b40241266c
Alisha Evans <alisha@notch8.com>
2022-11-30 10:48:59 -0600

    add note about adding future actions

--- src/compounds/ActionsGroup/ActionsGroup.jsx
+++ src/compounds/ActionsGroup/ActionsGroup.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import ListGroup from 'react-bootstrap/ListGroup'
 import './actions-group.scss'
+// NOTE(alishaevn): when adding more actions in the future, list each action as an additional list group item
+// and then add the new action component as a conditional underneath the list group
+// future actions may be disabled in certain request states as well so reger to https://react-bootstrap.github.io/components/list-group/
 
 const ActionsGroup = () => {
   const [show, setShow] = useState(false)
