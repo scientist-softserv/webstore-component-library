a7fcfcfe0e727ab7aef2cc7e3bb54b979533e239
Alisha Evans <alisha@notch8.com>
2022-11-30 17:39:13 -0600

    you can type a message that gets passed on from SendMessage

--- src/compounds/ActionsGroup/ActionsGroup.jsx
+++ src/compounds/ActionsGroup/ActionsGroup.jsx
@@ -1,18 +1,16 @@
 import React, { useState } from 'react'
-import ListGroup from 'react-bootstrap/ListGroup'
+import PropTypes from 'prop-types'
+import { ListGroup } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import SendMessage from './actions/SendMessage'
 import './actions-group.scss'
-// NOTE(alishaevn): when adding more actions in the future, list each action as an additional list group item
-// and then add the new action component as a conditional underneath the list group
-// future actions may be disabled in certain request states as well so reger to https://react-bootstrap.github.io/components/list-group/
 
-const ActionsGroup = () => {
+const ActionsGroup = ({ handleSendingMessages }) => {
   const [show, setShow] = useState(false)
   const [action, setAction] = useState(null)
 
-  const handleShow = (action) => {
-    setAction(action)
+  const handleShow = (componentName) => {
+    setAction(componentName)
     setShow(true)
   }
 
@@ -29,9 +27,19 @@ const ActionsGroup = () => {
           Send Message
         </ListGroup.Item>
       </ListGroup>
-      {(action === 'SendMessage' && show) && <SendMessage handleClose={handleClose} />}
+      {(action === 'SendMessage' && show)
+        && (
+          <SendMessage
+            handleClose={handleClose}
+            onSubmit={handleSendingMessages}
+          />
+        )}
     </>
   )
+}
+
+ActionsGroup.propTypes = {
+  handleSendingMessages: PropTypes.func({}).isRequired,
 }
 
 export default ActionsGroup
