82aefb6451bf6a2eda672252ca91c4502d338156
Summer Cook <summer@scientist.com>
2022-12-07 16:44:53 -0600

    small cleanup

--- src/compounds/AddressDetails/AddressDetails.jsx
+++ src/compounds/AddressDetails/AddressDetails.jsx
@@ -4,99 +4,95 @@ import { CountryDropdown } from 'react-country-region-selector';
 import PropTypes from 'prop-types'
 
 
-const AddressForm = ({ shippingEqualsBilling, shippingOrBillingString, toggleBilling }) => {
+const AddressForm = ({ shippingOrBillingString, toggleBilling, handleSubmit }) => {
   const [shippingCountry, setShippingCountry] = useState('');
   const [billingCountry, setBillingCountry] = useState('');
 
   // TODO: @summer-cook
   // add anything needed to save the values so they can be passed to the webstore
   return (
-      <Card.Body>
-        <Form>
-          {shippingOrBillingString == 'Shipping' && (
-            <Form.Group className='mb-5' controlId='address-name'>
-              <Form.Control placeholder='Address Name' name='addressName'/>
-            </Form.Group>
-          )}
+    <Card.Body>
+      <Form onSubmit={handleSubmit} id={`${shippingOrBillingString.toLowerCase()}-form`}>
+        {shippingOrBillingString == 'Shipping' && (
+          <Form.Group className='mb-5' controlId='address-name'>
+            <Form.Control placeholder='Address Name' name='addressName'/>
+          </Form.Group>
+        )}
 
-          <Card.Title className='mb-3'>{shippingOrBillingString} Address</Card.Title>
+        <Card.Title className='mb-3'>{shippingOrBillingString} Address</Card.Title>
 
-          <Form.Group className='mb-3' controlId={`address1-${shippingOrBillingString.toLowerCase()}`} name={`address1`}>
-            <Form.Control placeholder='Address' />
+        <Form.Group className='mb-3' controlId={`address1-${shippingOrBillingString.toLowerCase()}`}>
+          <Form.Control placeholder='Address' />
+        </Form.Group>
+
+        <Form.Group className='mb-3' controlId={`address2-${shippingOrBillingString.toLowerCase()}`}>
+          <Form.Control placeholder='Address Line 2 (optional)' />
+        </Form.Group>
+
+        <Form.Group className='mb-3' controlId={`city-${shippingOrBillingString.toLowerCase()}`}>
+          <Form.Control placeholder='City/Region' />
+        </Form.Group>
+
+        <Row className='mb-3'>
+          <Form.Group as={Col} controlId={`state-${shippingOrBillingString.toLowerCase()}`}>
+            <Form.Control placeholder='State/Province'/>
           </Form.Group>
 
-          <Form.Group className='mb-3' controlId={`address2-${shippingOrBillingString.toLowerCase()}`}>
-            <Form.Control placeholder='Address Line 2 (optional)' />
+          <Form.Group as={Col} controlId={`zip-${shippingOrBillingString.toLowerCase()}`}>
+            <Form.Control placeholder='Zip/Postal Code'/>
           </Form.Group>
+        </Row>
 
-          <Form.Group className='mb-3' controlId={`city-${shippingOrBillingString.toLowerCase()}`}>
-            <Form.Control placeholder='City/Region' />
+        <CountryDropdown
+          name={`country-${shippingOrBillingString.toLowerCase()}`}
+          priorityOptions={['US', 'GB', 'CA']}
+          value={shippingOrBillingString === 'Shipping' ? shippingCountry : billingCountry}
+          onChange={shippingOrBillingString === 'Shipping' ? (val) => setShippingCountry(val) : (val) => setBillingCountry(val)}
+          className='form-select mb-3 form-control'
+          id={`country-${shippingOrBillingString.toLowerCase()}`}
+        />
+
+        {shippingOrBillingString == 'Shipping' && (
+          <Form.Group className='mb-3' controlId='shippingEqualsBilling'>
+            <Form.Check 
+              type='checkbox'
+              label='My shipping address is the same as my billing address.'
+              onChange={toggleBilling}
+            />
           </Form.Group>
-
-          <Row className='mb-3'>
-            <Form.Group as={Col} controlId={`state-${shippingOrBillingString.toLowerCase()}`}>
-              <Form.Control placeholder='State/Province'/>
-            </Form.Group>
-
-            <Form.Group as={Col} controlId={`zip-${shippingOrBillingString.toLowerCase()}`}>
-              <Form.Control placeholder='Zip/Postal Code'/>
-            </Form.Group>
-          </Row>
-          {/* the country dropdown is displaying the countries correctly, but there are several issues that need to be addressed.
-          - theres might be a better way to manage the state as opposed to using 2 separate state hooks for shipping and billing countries
-          */}
-          <CountryDropdown
-            name={`country-${shippingOrBillingString.toLowerCase()}`}
-            priorityOptions={['US', 'GB', 'CA']}
-            value={shippingOrBillingString === 'Shipping' ? shippingCountry : billingCountry}
-            onChange={shippingOrBillingString === 'Shipping' ? (val) => setShippingCountry(val) : (val) => setBillingCountry(val)}
-            className='form-select mb-3 form-control'
-            id={`country-${shippingOrBillingString.toLowerCase()}`}
-          />
-
-          {shippingOrBillingString == 'Shipping' && (
-            <Form.Group className='mb-3' controlId='shippingEqualsBilling'>
-              <Form.Check 
-                type='checkbox'
-                label='My shipping address is the same as my billing address.'
-                onChange={toggleBilling}
-                checked={shippingEqualsBilling}
-              />
-            </Form.Group>
-          )}
-        </Form>
-      </Card.Body>
+        )}
+      </Form>
+    </Card.Body>
   )
 }
 
-const AddressDetails = () => {
+const AddressDetails = ({handleSubmit}) => {
   const [shippingEqualsBilling, setShippingEqualsBilling] = useState(false)
-  //const [childData, setChildData] = useState("");
 
   const toggleBilling = () => {
     setShippingEqualsBilling(shippingEqualsBilling => !shippingEqualsBilling);
   }
 
   return (
-    <>
-      <Card className='mb-4'>
-        <Card.Header className='h3'>Shipping Details</Card.Header>
+    <Card className='mb-4'>
+      <Card.Header className='h3'>Shipping Details</Card.Header>
+      <AddressForm
+        shippingOrBillingString='Shipping' 
+        shippingEqualsBilling={shippingEqualsBilling}
+        toggleBilling={toggleBilling}
+        handleSubmit={handleSubmit}
+      />
+      {shippingEqualsBilling === false && (
         <AddressForm
-          shippingOrBillingString='Shipping' 
+          shippingOrBillingString='Billing'
           shippingEqualsBilling={shippingEqualsBilling}
-          toggleBilling={toggleBilling}
+          handleSubmit={handleSubmit}
         />
-        {shippingEqualsBilling === false && (
-          <AddressForm
-            shippingOrBillingString='Billing'
-            shippingEqualsBilling={shippingEqualsBilling}
-          />
-        )}
-        <Button variant="primary" type="submit">
-            Submit
-        </Button>
-      </Card>
-    </>
+      )}
+      <Button variant="primary" type="submit" className='m-2'>
+          Submit
+      </Button>
+    </Card>
   )
 }
 
