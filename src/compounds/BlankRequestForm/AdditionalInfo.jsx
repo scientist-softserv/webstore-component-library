e7c97fbeb2654e5174906de8a8e2a150a5851566
Summer Cook <summer@scientist.com>
2022-12-08 15:05:32 -0600

    move everything into the blank request form

---
+++ src/compounds/BlankRequestForm/AdditionalInfo.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import { Card, Form } from 'react-bootstrap';
+import PropTypes from 'prop-types'
+
+
+const AdditionalInfo = ({openProposalDueDate, toggleOpenProposalDueDate}) => {
+  return (
+    <Card className='mb-4'>
+      <Card.Header className='h3'>Additional Information</Card.Header>
+      <Card.Body>
+      <Form.Group className='mb-3' controlId='project-code'>
+        <Form.Control placeholder='Project Code' />
+      </Form.Group>
+      <Form.Group 
+        className='mb-3'
+        controlId='required-by'>
+        <Form.Control
+          placeholder='Proposals Required By'
+          disabled={openProposalDueDate === true ? true : false}
+        />
+      </Form.Group>
+      <Form.Group
+        className='mb-3'
+        controlId='open-proposal-due-date'
+      >
+        <Form.Check 
+          type='checkbox'
+          label='Proposals can be submitted at any time.'
+          onChange={toggleOpenProposalDueDate}
+        />
+      </Form.Group>
+      <Form.Group controlId="attachments" className="mb-3">
+        <Form.Label>Attachments:</Form.Label>
+        <Form.Control type="file" multiple />
+      </Form.Group>
+    </Card.Body>
+  </Card>
+  )
+}
+
+
+AdditionalInfo.propTypes = {
+}
+
+AdditionalInfo.defaultProps = {
+}
+
+export default AdditionalInfo
---
+++ src/compounds/BlankRequestForm/BlankRequestForm.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import Title from '../../components/Title/Title'
+import ShippingDetails from './ShippingDetails'
+import AdditionalInfo from './AdditionalInfo';
+
+const BlankRequestForm = () => {
+  // used in the shipping details component
+  const [shippingEqualsBilling, setShippingEqualsBilling] = useState(false)
+  const [shippingValues, setShippingValues] = useState('');
+  const [billingValues, setBillingValues] = useState('');
+
+  // used in the additional info component
+  const [openProposalDueDate, setOpenProposalDueDate] = useState()
+
+  const toggleBilling = () => {
+    setShippingEqualsBilling(shippingEqualsBilling => !shippingEqualsBilling);
+    if (shippingEqualsBilling === true) {
+      setBillingValues(shippingValues)
+    }
+  }
+
+  const toggleOpenProposalDueDate = () => {
+    setOpenProposalDueDate(openProposalDueDate => !openProposalDueDate);
+  }
+
+  const handleSubmit = () => {
+
+  }
+
+  return (
+    <>
+      <Title title='New Request'/>
+      <Form onSubmit={handleSubmit} id='new-request-form'>
+        <Row className='mt-4 mb-2'>
+          <Form.Group className='mb-3' controlId='service-product-description'>
+            <Form.Label>Enter a service or product description.</Form.Label>
+            <Form.Control as='textarea' rows={3} />
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='project-timeline'>
+            <Form.Label>What is the timeline for this project?</Form.Label>
+            <Form.Control as='textarea' rows={3} />
+          </Form.Group>
+        </Row>
+        <Row>
+          <Col>
+            <ShippingDetails
+              toggleBilling={toggleBilling}
+              shippingEqualsBilling={shippingEqualsBilling}
+            />
+          </Col>
+          <Col>
+            <AdditionalInfo
+              toggleOpenProposalDueDate={toggleOpenProposalDueDate}
+              openProposalDueDate={openProposalDueDate}
+            />
+          </Col>
+        </Row>
+        <Button
+          variant='primary'
+          type='submit'
+          className='my-2 ms-auto d-block'
+          size='lg'>
+            Initiate Request
+        </Button>
+      </Form>
+    </>
+  )
+}
+
+BlankRequestForm.propTypes = {
+  
+}
+
+BlankRequestForm.defaultProps = {
+  
+}
+
+export default BlankRequestForm
