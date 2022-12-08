37e11df9a0596b6e58f56e1517c854fcdf590cf2
Alisha Evans <alisha@notch8.com>
2022-12-08 17:12:10 -0600

    using the react bs card component to render request stats

--- src/compounds/RequestStats/RequestStats.jsx
+++ src/compounds/RequestStats/RequestStats.jsx
@@ -1,56 +1,48 @@
 import React from 'react'
+import { Card } from 'react-bootstrap'
 import PropTypes from 'prop-types'
-import TextBox from '../../components/TextBox/TextBox'
-import Title from '../../components/Title/Title'
 import './request-stats.css'
 
-const RequestStats = ({ billingAddress, createdAt, deadline, detailsColor, projectCode, shippingAddress, headerColor }) => {
-  const titleStyle = { color: headerColor }
-  const textStyle = { color: detailsColor }
+const RequestStats = ({ billingAddress, createdAt, deadline, shippingAddress }) => {
+  const shippingLines = shippingAddress.split(/\n/)
+  const billingLines = billingAddress.split(/\n/)
 
   return (
-  <article className='request-stats'>
-    <div className='mb-2'>
-      <Title title='Request Info' size='small' />
-    </div>
-    <div className='mb-2'>
-      <Title title='Date of request' size='x-small' style={titleStyle} />
-      <TextBox text={createdAt} style={textStyle} />
-    </div>
-    <div className='mb-2'>
-      <Title title='Proposals required by' size='x-small' style={titleStyle} />
-      <TextBox text={deadline} style={textStyle} />
-    </div>
-    <div className='mb-2'>
-      <Title title='Project code' size='x-small' style={titleStyle} />
-      <TextBox text={projectCode} style={textStyle} />
-    </div>
-    <div className='mb-2'>
-      <Title title='Shipping address' size='x-small' style={titleStyle} />
-      <TextBox text={shippingAddress} style={textStyle} />
-    </div>
-    <div>
-      <Title title='Billing address' size='x-small' style={titleStyle} />
-      <TextBox text={billingAddress} style={textStyle} />
-    </div>
-  </article>
-)}
+    <Card className='w-25'>
+      <Card.Header>
+        <Card.Title>Request Info</Card.Title>
+      </Card.Header>
+      <Card.Body>
+        <Card.Subtitle>Date of request</Card.Subtitle>
+        <Card.Text className='mb-4'>{createdAt}</Card.Text>
+        <Card.Subtitle>Proposals required by</Card.Subtitle>
+        <Card.Text className='mb-4'>{deadline}</Card.Text>
+        <Card.Subtitle>Shipping Address</Card.Subtitle>
+        {shippingLines.map((line, index) => (
+          <Card.Text className={`${(index === shippingLines.length-1) ? 'mb-4' : 'mb-0'}`}>
+            {line}
+          </Card.Text>)
+        )}
+        <Card.Subtitle>Billing Address</Card.Subtitle>
+        {billingLines.map((line, index) => (
+          <Card.Text className={`${(index === billingLines.length-1) ? '' : 'mb-0'}`}>
+            {line}
+          </Card.Text>)
+        )}
+      </Card.Body>
+    </Card>
+  )
+}
 
 RequestStats.propTypes = {
   billingAddress: PropTypes.string.isRequired,
   createdAt: PropTypes.string.isRequired,
   deadline: PropTypes.string,
-  detailsColor: PropTypes.string,
-  headerColor: PropTypes.string,
-  projectCode: PropTypes.string,
   shippingAddress: PropTypes.string.isRequired,
 }
 
 RequestStats.defaultProps = {
   deadline: '',
-  detailsColor: '#999999',
-  headerColor: '#333333',
-  projectCode: '',
 }
 
 export default RequestStats
