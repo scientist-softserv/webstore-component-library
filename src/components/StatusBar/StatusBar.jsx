34cfc322addb30cdb9948e5b868e5c399dc278b6
Summer Cook <summer@scientist.com>
2022-12-15 14:54:48 -0600

    add an addClass option to status bar

--- src/components/StatusBar/StatusBar.jsx
+++ src/components/StatusBar/StatusBar.jsx
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './status-bar.scss'
 
-const StatusBar = ({ statusArray, apiRequestStatus }) => (
-  <div className='container'>
+const StatusBar = ({ statusArray, apiRequestStatus, addClass }) => (
+  <div className={`container ${addClass}`}>
     <div className='row border bg-light'>
       {statusArray.map((statusObject, index) => {
         const { statusLabel, statusIcon } = statusObject
@@ -27,6 +27,11 @@ const StatusBar = ({ statusArray, apiRequestStatus }) => (
 StatusBar.propTypes = {
   statusArray: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
   apiRequestStatus: PropTypes.string.isRequired,
+  addClass: PropTypes.string,
+}
+
+StatusBar.defaultProps = {
+  addClass: '',
 }
 
 export default StatusBar
