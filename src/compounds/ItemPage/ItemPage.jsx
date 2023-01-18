a53dcd482b2651cb63a3a7de33d6a633d8d595ff
Summer Cook <summer@scientist.com>
2023-01-18 11:41:29 -0600

    use the full ware instead of all different props, fix linting errors

--- src/compounds/ItemPage/ItemPage.jsx
+++ src/compounds/ItemPage/ItemPage.jsx
@@ -6,24 +6,24 @@ import LinkedButton from '../LinkedButton/LinkedButton'
 import TextBox from '../../components/TextBox/TextBox'
 import Title from '../../components/Title/Title'
 
-const ItemPage = ({ title, titleStyle, description, descriptionStyle, id, img, slug }) => {
+const ItemPage = ({ descriptionStyle, img, ware, titleStyle }) => {
+  const { name, description, id, snippet, slug } = ware
   // sets a default width while still allowing width to be overridden
-  img = { width: 400, ...img }
 
   return (
     <main className='container py-5'>
-      <Title title={title} style={titleStyle}/>
+      <Title title={name} style={titleStyle} />
       <Row className='mt-3'>
         <Col xs={12} md={6} className='d-flex flex-column'>
-          <TextBox text={description} style={descriptionStyle} size='medium' />
+          <TextBox text={description || snippet} style={descriptionStyle} size='medium' />
           <LinkedButton
             addClass='align-self-start mt-4'
             buttonProps={{ label: 'Initiate Request' }}
-            path={{ pathname: `/requests/new/${slug}`, query: { id }}}
+            path={{ pathname: `/requests/new/${slug}`, query: { id } }}
           />
         </Col>
         <Col xs={12} md={6} className='mt-4 mt-md-0 me-md-auto d-flex justify-content-start justify-content-md-end'>
-          <Image {...img} />
+          <Image {...img} addClass='img-fluid'/>
         </Col>
       </Row>
     </main>
@@ -31,16 +31,19 @@ const ItemPage = ({ title, titleStyle, description, descriptionStyle, id, img, s
 }
 
 ItemPage.propTypes = {
-  description: PropTypes.string.isRequired,
   descriptionStyle: PropTypes.shape({}),
-  id: PropTypes.number.isRequired,
   img: PropTypes.shape({
     alt: PropTypes.string.isRequired,
     src: PropTypes.string.isRequired,
-  }),
-  slug: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
+  }).isRequired,
   titleStyle: PropTypes.shape({}),
+  ware: PropTypes.shape({
+    description: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    slug: PropTypes.string.isRequired,
+    snippet: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
 }
 
 ItemPage.defaultProps = {
