49920abcbd1971bc731cfff66d1d0da805602f81
Summer Cook <summer@scientist.com>
2023-01-20 08:38:28 -0600

    remove dismissable since this will be the only thing that shows on the page

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,38 +1,36 @@
 /* eslint react/no-array-index-key: 0 */
-import React, { useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { Alert, Button, Container } from 'react-bootstrap'
 
 const Error = ({ errorText, errorTitle, router, variant }) => {
-  const [show, setShow] = useState(true);
-  if (show) {
-    return (
-        <Container>
-          <Alert className='mt-5 text-break' variant={variant} onClose={() => setShow(false)}>
-            {errorTitle && (
-              <Alert.Heading>{errorTitle}</Alert.Heading>
-            )}
-            {errorText && (errorText)}
-            <hr/>
-            <div className="d-flex justify-content-end">
-              <Button onClick={() => router.back()} variant={`outline-${variant}`}>
-                Click to return to the previous page
-              </Button>
-            </div>
-          </Alert>
-        </Container>
-    )
-  }
+  return (
+    <Container>
+      <Alert className='mt-5 text-break' variant={variant}>
+        {errorTitle && (
+          <Alert.Heading>{errorTitle}</Alert.Heading>
+        )}
+        {errorText && (errorText)}
+        <hr />
+        <div className='d-flex justify-content-end'>
+          <Button onClick={() => router.back()} variant={`outline-${variant}`}>
+            Click to return to the previous page.
+          </Button>
+        </div>
+      </Alert>
+    </Container>
+  )
 }
 
 Error.propTypes = {
   errorText: PropTypes.string.isRequired,
   errorTitle: PropTypes.string,
+  router: PropTypes.shape({}).isRequired,
   variant: PropTypes.string.isRequired,
 }
 
 Error.defaultProps = {
-  errorTitle: ''
+  errorTitle: '',
 }
 
 export default Error
