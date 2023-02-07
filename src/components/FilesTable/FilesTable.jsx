60dee939fe59a8821bbc6da9de2348df3a027226
Summer Cook <summer@scientist.com>
2023-02-06 21:28:35 -0600

    fix initial files proptype, linting for story

--- src/components/FilesTable/FilesTable.jsx
+++ src/components/FilesTable/FilesTable.jsx
@@ -57,19 +57,21 @@ const FilesTable = ({ addClass, files }) => {
 
 FilesTable.propTypes = {
   addClass: PropTypes.string,
-  files: PropTypes.shape([{
-    contentLength: PropTypes.string.isRequired,
-    content_length: PropTypes.number.isRequired,
-    content_type: PropTypes.string.isRequired,
-    createdAt: PropTypes.string.isRequired,
-    created_at: PropTypes.string.isRequired,
-    download: PropTypes.string.isRequired,
-    filename: PropTypes.string.isRequired,
-    href: PropTypes.string.isRequired,
-    status: PropTypes.string.isRequired,
-    uploadedBy: PropTypes.string.isRequired,
-    uuid: PropTypes.string.isRequired,
-  }]).isRequired,
+  files: PropTypes.arrayOf(
+    PropTypes.shape({
+      contentLength: PropTypes.string.isRequired,
+      content_length: PropTypes.number.isRequired,
+      content_type: PropTypes.string.isRequired,
+      createdAt: PropTypes.string.isRequired,
+      created_at: PropTypes.string.isRequired,
+      download: PropTypes.string.isRequired,
+      filename: PropTypes.string.isRequired,
+      href: PropTypes.string.isRequired,
+      status: PropTypes.string.isRequired,
+      uploadedBy: PropTypes.string.isRequired,
+      uuid: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
   // TODO(summer-cook): add this back in. see comment above.
   // handleDeleteFile: PropTypes.func.isRequired,
 }
--- src/compounds/ActionsGroup/actions/ViewFiles.jsx
+++ src/compounds/ActionsGroup/actions/ViewFiles.jsx
@@ -133,7 +133,21 @@ const ViewFiles = ({ initialFiles, handleClose, show }) => {
 }
 
 ViewFiles.propTypes = {
-  initialFiles: PropTypes.shape([{}]).isRequired,
+  initialFiles: PropTypes.arrayOf(
+    PropTypes.shape({
+      contentLength: PropTypes.string.isRequired,
+      content_length: PropTypes.number.isRequired,
+      content_type: PropTypes.string.isRequired,
+      createdAt: PropTypes.string.isRequired,
+      created_at: PropTypes.string.isRequired,
+      download: PropTypes.string.isRequired,
+      filename: PropTypes.string.isRequired,
+      href: PropTypes.string.isRequired,
+      status: PropTypes.string.isRequired,
+      uploadedBy: PropTypes.string.isRequired,
+      uuid: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
   handleClose: PropTypes.func.isRequired,
   show: PropTypes.bool.isRequired,
 }
