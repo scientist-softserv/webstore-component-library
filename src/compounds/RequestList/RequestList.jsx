2e2fd9a3f654088cf786baaa08eeb0cba582bfa6
Alisha Evans <alisha@notch8.com>
2023-02-15 13:50:13 -0600

    dynamically set the bg color of the request items in the request list

--- src/compounds/RequestList/RequestList.jsx
+++ src/compounds/RequestList/RequestList.jsx
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types'
 import Title from '../../components/Title/Title'
 import RequestItem, { requestPropTypes } from '../RequestItem/RequestItem'
 
-const RequestList = ({ requests }) => (
+const RequestList = ({ backgroundColor, requests }) => (
   <>
     <Title title='My Requests' size='medium' />
     <div className='rounded overflow-hidden mb-4'>
       {requests.map((req, index) => (
         <Link key={req.id} href={`${req.href}`} passHref legacyBehavior>
-          <RequestItem request={req} index={index} />
+          <RequestItem request={req} index={index} backgroundColor={backgroundColor} />
         </Link>
       ))}
     </div>
@@ -18,7 +18,12 @@ const RequestList = ({ requests }) => (
 )
 
 RequestList.propTypes = {
+  backgroundColor: PropTypes.string,
   requests: PropTypes.arrayOf(PropTypes.shape(requestPropTypes)).isRequired,
+}
+
+RequestList.defaultProps = {
+  backgroundColor: 'light'
 }
 
 export default RequestList
--- src/compounds/RequestList/RequestList.stories.jsx
+++ src/compounds/RequestList/RequestList.stories.jsx
@@ -41,3 +41,37 @@ Default.args = {
     },
   ],
 }
+
+export const Alternate = Template.bind({})
+Alternate.args = {
+  backgroundColor: 'secondary',
+  requests: [
+    {
+      createdAt: 'September 9, 2022',
+      description: 'Does the Company offer services related to Flow Cytometry?',
+      href: '/request/F575C4',
+      id: 1,
+      img: defaultImage,
+      status: {
+        text: 'Vendor Review',
+      },
+      title: 'F575C4: Assay Depot Coffee Mug',
+      updatedAt: 'September 9, 2022 at 9:21 am',
+    },
+    {
+      createdAt: 'November 15, 2022',
+      // eslint-disable-next-line max-len
+      description: 'General Information When do you plan to work with this supplier? Urgently Name of supplier: Alisha Supplier web address: http://scientist.com Is this new supplier onboarding request related to any of the following areas: Research area: In Vivo Contact Information Supplier contact name: Alisha Evans Supplier email a...',
+      href: '/request/706D8F',
+      id: 2,
+      img: defaultImage,
+      status: {
+        backgroundColor: '#DEAF17',
+        text: 'Work In Progress',
+        textColor: '#FFFFFF',
+      },
+      title: '706D8F: CRISPR',
+      updatedAt: 'November 16, 2022 at 4:45 pm',
+    },
+  ],
+}
