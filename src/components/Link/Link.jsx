48bd8a2f3d7def790fa6112a5dbab875b48cb3ca
Summer Cook <summer@scientist.com>
2023-02-21 10:05:20 -0500

    enable download, remove margin on link if there is no label

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -11,7 +11,7 @@ const Link = ({ addClass, href, icon, label, style, ...props }) => (
     {...props}
   >
     {icon && (
-      <FontAwesomeIcon icon={icon} className='me-2 small' />
+      <FontAwesomeIcon icon={icon} className={`small ${label.length > 0 && 'me-2'}`} />
     )}
     {label}
   </a>
