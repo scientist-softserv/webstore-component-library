0f700ede7b4521e2b2e848e949853528c3f60ec5
Alisha Evans <alisha@scientist.com>
2023-03-08 12:44:41 -0600

    wip refactor

--- src/compounds/Document/Document.jsx
+++ src/compounds/Document/Document.jsx
@@ -50,7 +50,7 @@ const Document = ({ acceptSOW, addClass, backgroundColor, document }) => {
                     <Dropdown.Item
                       href='#/action-1'
                       onClick={() => {
-                        acceptSOW
+                        onClick('Accept SOW')
                         setShowNotice(true)
                       }}
                     >
