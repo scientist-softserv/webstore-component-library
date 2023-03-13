1ed5a01d43a7197688a79885fbbe2b1dbcf15942
Diem Tran <diembtran@gmail.com>
2023-03-13 11:49:06 -0700

    passing datacy for home page test

--- src/compounds/TitledTextBox/TitledTextBox.jsx
+++ src/compounds/TitledTextBox/TitledTextBox.jsx
@@ -3,14 +3,15 @@ import PropTypes from 'prop-types'
 import { TextBox, Title } from '../../components'
 import './titled-text-box.css'
 
-const TitledTextBox = ({ text, textAlignment, textSize, textStyle, title, titleAlignment, titleSize, titleStyle }) => (
-  <section className='titled-text-box'>
+const TitledTextBox = ({ dataCy, text, textAlignment, textSize, textStyle, title, titleAlignment, titleSize, titleStyle }) => (
+  <section className='titled-text-box' data-cy={dataCy}>
     <Title addClass='mb-2' alignment={titleAlignment} size={titleSize} style={titleStyle} title={title} />
     <TextBox alignment={textAlignment} size={textSize} style={textStyle} text={text} />
   </section>
 )
 
 TitledTextBox.propTypes = {
+  dataCy: PropTypes.string,
   text: PropTypes.string.isRequired,
   textAlignment: PropTypes.oneOf(['left', 'center', 'right']),
   textSize: PropTypes.oneOf(['small', 'medium', 'large']),
@@ -22,6 +23,7 @@ TitledTextBox.propTypes = {
 }
 
 TitledTextBox.defaultProps = {
+  dataCy: '',
   textAlignment: 'left',
   textSize: 'medium',
   textStyle: {},
