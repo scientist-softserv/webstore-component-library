566cbfcab09014f2e85e5465b1b1decb829b564c
Summer Cook <summer@scientist.com>
2024-02-01 09:39:07 -0800

    revert cardbody

--- src/compounds/Item/CardBody.jsx
+++ src/compounds/Item/CardBody.jsx
@@ -4,67 +4,52 @@ import { Card } from 'react-bootstrap'
 import NextLink from '../../components/NextLink/NextLink'
 import LinkedButton from '../LinkedButton/LinkedButton'
 
-const CardBody = ({
-  buttonLink,
-  buttonProps,
-  item,
-  orientation,
-  titleLink,
-  withButtonLink,
-  withTitleLink,
-}) => {
-  const { id, description, name } = item;
+const CardBody = ({ buttonLink, buttonProps, item,
+  orientation, titleLink, withButtonLink, withTitleLink }) => {
+  const { id, description, name } = item
 
   return (
-    <Card.Body className={withButtonLink && "d-flex flex-column"}>
-        <div
-          className={
-            orientation === "horizontal"
-              ? "d-block d-md-flex align-items-center justify-content-between"
-              : ""
-          }
-        >
-          <div className={orientation === "horizontal" ? "me-2" : ""}>
-            <Card.Title >
+    <Card.Body className={withButtonLink && 'd-flex flex-column'}>
+      <div className={orientation === 'horizontal' ? 'd-block d-md-flex align-items-center justify-content-between' : ''}>
+        <div className={orientation === 'horizontal' ? 'me-2' : ''}>
+          <Card.Title>
             {(withTitleLink) && (
               <NextLink
-              text={name}
-              path={{ pathname: titleLink, query: { id } }}
-              addClass="text-decoration-none link-hover"
-            />)}
-              {(!withTitleLink) && (
-                name
+                text={name}
+                path={{ pathname: titleLink, query: { id } }}
+                addClass='text-decoration-none link-hover'
+              />
+            )}
+            {(!withTitleLink) && (
+              name
             )}
           </Card.Title>
-            {description && (
-              <Card.Text className="fw-light">{description}</Card.Text>
-            )}
-          </div>
-          {withButtonLink && (
-            <div
-              className={orientation === "horizontal" ? "mt-3 mt-md-0" : "mt-3"}
-            >
-              <LinkedButton
-                addClass={`item-button-${orientation} item-link mt-auto`}
-                buttonProps={buttonProps}
-                path={{ pathname: buttonLink, query: { id } }}
-              />
-            </div>
+          {description && (
+            <Card.Text className='fw-light'>
+              {description}
+            </Card.Text>
           )}
         </div>
-      </Card.Body>
-  );
-};
+        {(withButtonLink) && (
+          <div className={orientation === 'horizontal' ? 'mt-3 mt-md-0' : 'mt-3'}>
+            <LinkedButton
+              addClass={`item-button-${orientation} item-link mt-auto`}
+              buttonProps={buttonProps}
+              path={{ pathname: buttonLink, query: { id }}}
+            />
+          </div>
+        )}
+      </div>
+    </Card.Body>
+  )
+}
 
 CardBody.propTypes = {
   buttonLink: PropTypes.string,
   // currently overriding the label on a button from being required in this component,
   // because it shouldn't be if we are not rendering a button
   // refer to the comment below
-  buttonProps: PropTypes.shape({
-    ...LinkedButton.propTypes.buttonProps,
-    label: PropTypes.string,
-  }),
+  buttonProps: PropTypes.shape({ ...LinkedButton.propTypes.buttonProps, label: PropTypes.string }),
   // TODO(alishaevn): is there a way to set conditional proptypes without adding another package?
   // buttonProps: props => props.withButtonLink
   // ? PropTypes.shape(Button.propTypes)
@@ -83,7 +68,7 @@ CardBody.propTypes = {
   titleLink: PropTypes.string,
   withButtonLink: PropTypes.bool,
   withTitleLink: PropTypes.bool,
-};
+}
 
 CardBody.defaultProps = {
   buttonLink: '',
@@ -95,6 +80,6 @@ CardBody.defaultProps = {
   titleLink: '',
   withButtonLink: false,
   withTitleLink: false,
-};
+}
 
-export default CardBody;
+export default CardBody
